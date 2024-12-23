28d1c187ea288008e5569f8b946a2a2c178ccf2b
Gus Mueller <gus@asecular.com>
2024-12-22 20:56:00 -0500

    Update config.c

--- config.c
+++ config.c
@@ -9,5 +9,9 @@ const char* host_get = "randomsprocket.com";
 const int device_id = 16;
 const int connection_failure_retry_seconds = 4;
 const int connection_retry_number = 22;
+const char pins_to_start_low[] = {12, 13, -1}; //so when the device comes up it doesn't immediately turn on attached devices
 
-const int ir_pin = 14; //set to the value of an actual data pin if you want to send data via ir from your controller on occasi
+const int ir_pin = 14; //set to the value of an actual data pin if you want to send data via ir from your controller on occasion
+const int ina219_address = 0x40; //set to -1 if you have no power voltage to monitor
+const int ina219_address_a = -1; //set to -1 if you have no power voltage a to monitor
+const int ina219_address_b = -1; //set to -1 if you have no power voltage b to monitor
