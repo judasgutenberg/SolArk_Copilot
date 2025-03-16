1c17fb500fbe13d0bc382e28909eba83cfa1abcf
Gus Mueller <gus@asecular.com>
2025-03-16 19:45:47 -0400

    new encryption scheme

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 extern const char* wifi_ssid; //mine was Moxee Hotspot83_2.4G
 extern const char* wifi_password;
 extern const char* storage_password; //to ensure someone doesn't store bogus data on your server. should match value in config.php
-extern const char* salt;
+extern const unsigned long long encryption_scheme;
 //data posted to remote server so we can keep a historical record
 //url will be in the form: http://your-server.com:80/weather/data.php?data=
 extern const char* url_get;
